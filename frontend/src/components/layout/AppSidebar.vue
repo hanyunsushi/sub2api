90ddeeac13f2bc23c18d73f832be073674c94b40
hanyunsushi <2424791234@qq.com>
2026-05-18 17:41:53 +0800

    fix(frontend): smooth sidebar scroll tracking

--- frontend/src/components/layout/AppSidebar.vue
+++ frontend/src/components/layout/AppSidebar.vue
@@ -249,6 +249,8 @@ let shouldRestoreSidebarScroll = false
 let sidebarRestoreFrame = 0
 let sidebarRestoreTimeout = 0
 let sidebarRestoreAttempts = 0
+let latestSidebarScrollTop = appStore.sidebarNavScrollTop
+let sidebarScrollCommitFrame = 0
 
 // Site settings from appStore (cached, no flicker)
 const siteName = computed(() => appStore.siteName)
@@ -829,20 +831,49 @@ function closeMobile() {
 
 function captureSidebarScroll() {
   if (!sidebarNavRef.value) return
-  appStore.setSidebarNavScrollTop(sidebarNavRef.value.scrollTop)
+  latestSidebarScrollTop = sidebarNavRef.value.scrollTop
+  commitSidebarScrollTop()
   shouldRestoreSidebarScroll = true
 }
 
 function handleSidebarNavScroll() {
   if (!sidebarNavRef.value) return
-  appStore.setSidebarNavScrollTop(sidebarNavRef.value.scrollTop)
+  latestSidebarScrollTop = sidebarNavRef.value.scrollTop
+  scheduleSidebarScrollCommit()
+}
+
+function scheduleSidebarScrollCommit() {
+  if (sidebarScrollCommitFrame) return
+  sidebarScrollCommitFrame = requestAnimationFrame(() => {
+    sidebarScrollCommitFrame = 0
+    commitSidebarScrollTop()
+  })
+}
+
+function cancelSidebarScrollCommitSchedule() {
+  if (!sidebarScrollCommitFrame) return
+  cancelAnimationFrame(sidebarScrollCommitFrame)
+  sidebarScrollCommitFrame = 0
+}
+
+function commitSidebarScrollTop() {
+  appStore.setSidebarNavScrollTop(latestSidebarScrollTop)
+}
+
+function flushSidebarScrollCommit() {
+  if (sidebarScrollCommitFrame) {
+    cancelSidebarScrollCommitSchedule()
+    commitSidebarScrollTop()
+  }
 }
 
 function applySidebarScrollRestore() {
   if (!sidebarNavRef.value) return
+  flushSidebarScrollCommit()
   const previousScrollBehavior = sidebarNavRef.value.style.scrollBehavior
   sidebarNavRef.value.style.scrollBehavior = 'auto'
-  sidebarNavRef.value.scrollTop = appStore.sidebarNavScrollTop
+  latestSidebarScrollTop = appStore.sidebarNavScrollTop
+  sidebarNavRef.value.scrollTop = latestSidebarScrollTop
   if (previousScrollBehavior) {
     sidebarNavRef.value.style.scrollBehavior = previousScrollBehavior
   } else {
@@ -1012,6 +1043,7 @@ onMounted(() => {
 
 onBeforeUnmount(() => {
   captureSidebarScroll()
+  cancelSidebarScrollCommitSchedule()
   cancelSidebarScrollRestoreSchedule()
 })
 </script>
